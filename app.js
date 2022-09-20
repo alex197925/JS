ae405cb743286fc8920ae9c98d2795db90fcbc54
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-20 11:07:13 +0200

    JavaScript exercises

---
+++ app.js
@@ -0,0 +1,51 @@
+// const addNumbers = (num1, num2) => {
+//    return num1 + num2;
+// }
+//
+// console.log(addNumbers(5, 5));
+
+//Convert min to the seconds
+// function convert(min) {
+//     return min * 60;
+// }
+//
+// console.log(convert(1));
+// console.log(convert(2));
+
+// function yourAgeInSeconds(age) {
+//     //years * month (12) * days (30) * hours (24) * minutes (60) * seconds (60)
+//     return age * 12 * 30 * 24 * 60 * 60;
+// }
+
+// console.log(yourAgeInSeconds(43));
+
+
+// Get first item in array
+// 1. const items = ["banana", "GodFather", "ShawShnak",  "Picle"];
+// let firstItem = items.shift();
+// console.log("First item in array is: " + firstItem);
+
+// function getFirstItem (items) {
+//     return items[0];
+// }
+//
+// let fruits = [
+//     'banana', 'orange',
+//     'kiwi', 'apple',
+// ]
+//
+// console.log(getFirstItem(fruits));
+
+// function badOrGoodMovie (movie_rating) {
+//     if (movie_rating >= 7) {
+//        return "Good";
+//     } else {
+//         return "Bad";
+//     }
+// }
+//
+// console.log(badOrGoodMovie(7));
+// console.log(badOrGoodMovie(6));
+
+
+
